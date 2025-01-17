00627ed68fdade9ff1274eeaa71dd70b7c084d5c
Fuad <fuadreddhow@gmail.com>
2025-01-17 23:22:02 +0600

    implemented GET and PUT methods

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('products', views.product_list, name='products'),
+    path('product/<int:pk>', views.product_detail, name='product_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        
+        return Response(serializer.errors, status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
