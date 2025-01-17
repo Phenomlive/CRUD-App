2fa21062a84f425330484c89da60d2113a3108a2
Fuad <fuadreddhow@gmail.com>
2025-01-18 00:20:39 +0600

    cretaed serializers for user registration

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('products', views.product_list, name='products'),
     path('product/<int:pk>', views.product_detail, name='product_detail'),
+    path('register', views.register, name='register')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
 
 # Create your views here.
 
 @api_view(['GET', 'POST'])
-def product_list(request):
+def product_list(request, format=None):
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
@@ -19,12 +19,12 @@ def product_list(request):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, pk):
+def product_detail(request, pk, format=None):
     try:
         product = Product.objects.get(id=pk)
     except Product.DoesNotExist:
@@ -33,3 +33,27 @@ def product_detail(request, pk):
     if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    data = {}
+
+    if serializer.is_valid():
+        user = serializer.save()
+        data['response'] = "Successfully registered a new user."
+    else:
+        data = serializer.errors
